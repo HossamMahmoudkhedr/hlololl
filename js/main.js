855a6a94506b4e5bfe79d64ef8cbc04bdbe8885a
HossamMahmoudKhedr <nhem318@gmail.com>
2024-05-09 16:25:49 +0300

    add all modifications

--- js/main.js
+++ js/main.js
@@ -1,67 +1,35 @@
-// const sections = document.querySelectorAll('.section');
-// console.log(sections);
-// sections.forEach((section) => {
-// 	section.onclick = (e) => {
-// 		const target = e.target;
-// 		if (
-// 			target.classList.contains('pic') ||
-// 			target.classList.contains('img-responsive') ||
-// 			target.classList.contains('c_name')
-// 		) {
-// 			sections.forEach((section) => {
-// 				section.classList.remove('show');
-// 			});
-// 			section.classList.toggle('show');
-// 		}
-// 	};
-// });
-
 const form = document.querySelector('form');
 const searchBtn = document.getElementById('searchBtn');
 const downloadBtn = document.getElementById('downloadBtn');
 const downloadAlert = document.getElementById('downloadAlert');
 const showBook = document.getElementById('showBook');
-const viewBook = document.getElementById('viewBook');
+const book = document.getElementById('book');
 const sections = document.querySelectorAll('.section');
-const summaries = document.querySelectorAll('.summary');
+const pics = document.querySelectorAll('.pic');
+const black = document.querySelector('.black');
+const closeIcon = document.querySelectorAll('.closeIcon');
 
 const showItem = () => {
 	console.log('clicked');
 	form.classList.toggle('showItem');
 };
 
-sections.forEach((section, i) => {
-	section.onclick = (e) => {
-		const target = e.target;
-
-		if (
-			target.classList.contains('pic') ||
-			target.classList.contains('img-responsive') ||
-			target.classList.contains('c_name')
-		) {
-			sections.forEach((section, index) => {
-				if (i !== index) {
-					section.classList.remove('showItem');
-				}
-			});
-			section.classList.toggle('showItem');
-			summaries.forEach((summary, i) => {
-				if (
-					summary.getAttribute('data-id') === section.getAttribute('data-id')
-				) {
-					summaries.forEach((summary, index) => {
-						if (i !== index) {
-							summary.classList.remove('showItem');
-						}
-					});
-					summary.classList.toggle('showItem');
-				}
-			});
+const hidePopUp = (e) => {
+	sections.forEach((section) => {
+		if (section.classList.contains('showSummary')) {
+			section.classList.remove('showSummary');
 		}
+	});
+	black.classList.remove('showBlack');
+};
+
+pics.forEach((pic) => {
+	pic.onclick = () => {
+		pic.parentElement.classList.add('showSummary');
+		black.classList.add('showBlack');
 	};
 });
 
-searchBtn ? searchBtn.addEventListener('click', showItem) : '';
 if (downloadBtn && downloadAlert) {
 	downloadBtn.addEventListener('click', () => {
 		console.log('clicked');
@@ -72,20 +40,17 @@ if (downloadBtn && downloadAlert) {
 	});
 }
 
-if (showBook && viewBook) {
-	let curr = viewBook.innerHTML;
-	let content = `
-		<embed
-						src="./books/The_React__Axios_Cheatsheet.pdf"
-						type="application/pdf"
-						frameBorder="0"
-						scrolling="auto"
-						height="100%"
-						width="100%" />
-	`;
-	showBook.addEventListener('click', () => {
-		if (viewBook.innerHTML === curr) {
-			viewBook.innerHTML = content;
-		}
-	});
+const showBookContent = () => {
+	book.classList.add('viewBook');
+};
+
+if (showBook && book) {
+	showBook.addEventListener('click', showBookContent);
 }
+
+closeIcon.forEach((icon) => {
+	icon.addEventListener('click', hidePopUp);
+});
+
+black ? black.addEventListener('click', hidePopUp) : '';
+searchBtn ? searchBtn.addEventListener('click', showItem) : '';
